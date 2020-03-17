f7553490614466a1e527a56af1ce1627e30ee19f
chris_frendo <chirs.frendo.18@um.edu.mt>
2020-03-17 13:41:56 +0100

    Added tests

--- src/test/java/com/josephmasini/gitexercise/PatternRepeaterTest.java
+++ src/test/java/com/josephmasini/gitexercise/PatternRepeaterTest.java
@@ -1,7 +1,36 @@
 package com.josephmasini.gitexercise;
 
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class PatternRepeaterTest {
+
+    private PatternRepeater patternRepeater;
+
+    @Before
+    public void setUp() {
+        patternRepeater = new PatternRepeater();
+    }
 
     /* Implement tests */
 
+    @Test
+    public void sortIntegersTest() {
+        int[] integers = {10, 8, 2, 3};
+        int[] expected = {2, 3, 8, 10};
+        int[] result = patternRepeater.sortIntegers(integers);
+
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    public void sortStringTest() {
+        String[] integers = {"test", "chris", "abc"};
+        String[] expected = {"abc", "chris", "test"};
+        String[] result = patternRepeater.sortStrings(integers);
+
+        assertArrayEquals(expected, result);
+    }
 }
