801d23f3bbb36f3adc57e9b02f26aafed99af237
chris_frendo <chirs.frendo.18@um.edu.mt>
2020-03-17 13:43:19 +0100

    Added code

--- src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
+++ src/main/java/com/josephmasini/gitexercise/PatternRepeater.java
@@ -1,9 +1,17 @@
 package com.josephmasini.gitexercise;
 
+import java.util.Arrays;
+
 public class PatternRepeater {
 
     /* Implement method to sort an array of integers */
-
+    public int[] sortIntegers(int[] ints) {
+        Arrays.sort(ints);
+        return ints;
+    }
     /* Implement method to sort an array of strings */
-
+    public String[] sortStrings(String[] strs) {
+        Arrays.sort(strs);
+        return strs;
+    }
 }
